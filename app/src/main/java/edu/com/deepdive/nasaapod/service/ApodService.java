953cf788f959bf5b784e8265d8b18bc8d9a64f2c
mabbyreyes <mabbyreyes@gmail.com>
2020-01-28 16:56:25 -0700

    First version of interface to APOD service.

---
+++ app/src/main/java/edu/com/deepdive/nasaapod/service/ApodService.java
@@ -0,0 +1,13 @@
+package edu.com.deepdive.nasaapod.service;
+
+import edu.com.deepdive.nasaapod.model.Apod;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApodService {
+
+  @GET("planetary/apod")
+  Call<Apod> get(@Query("api_key") String apiKey, @Query("date") String date);
+
+}
